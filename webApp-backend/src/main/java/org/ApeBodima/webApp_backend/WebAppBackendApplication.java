209d5f0c2ce72dd2175b0527d2d8ed23c9c0f5c0
Sanjaya pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-06-24 19:23:50 +0530

    Project-env

---
+++ webApp-backend/src/main/java/org/ApeBodima/webApp_backend/WebAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ApeBodima.webApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppBackendApplication.class, args);
+	}
+
+}
