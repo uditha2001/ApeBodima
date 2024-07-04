726603ce8bf98e1c6ab057f98c5e7e4d4340bcf4
Sanjaya Pradeep <152323246+SPradeepIndie@users.noreply.github.com>
2024-07-04 20:53:16 +0530

    Revert "Completed  Add to Favourite list"

--- webApp-backend/src/main/java/org/ApeBodima/webApp_backend/repository/FavouriteBodimListRepo.java
+++
@@ -1,9 +0,0 @@
-package org.ApeBodima.webApp_backend.repository;
-
-import org.ApeBodima.webApp_backend.entity.FavouriteBodimList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FavouriteBodimListRepo extends JpaRepository<FavouriteBodimList, Long> {
-}
