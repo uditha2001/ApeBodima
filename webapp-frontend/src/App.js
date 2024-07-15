83c5400384ddb53814c3ac4eb438b719eb8e9b1b
tharaka <tharakam2001@gmail.com>
2024-07-15 20:33:03 +0530

    update the footer component

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderComponent from "./component/HeaderComponent"
+import FooterComponent from "./component/FooterComponent"
 
 const App=()=>{
     return(
         <div>
-        <HeaderComponent/>
+        
+        <FooterComponent/>
         
         </div>
     )
