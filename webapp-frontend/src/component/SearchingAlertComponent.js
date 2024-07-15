d3128400503dad78f943dfd0a30a2adde7f1ffcd
Sanjaya pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-07-15 15:18:05 +0530

    Complete the Searching alert component

--- webapp-frontend/src/component/SearchingAlertComponent.js
+++ webapp-frontend/src/component/SearchingAlertComponent.js
@@ -1,49 +1,40 @@
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import {Typography} from '@mui/material';
 import SearchingImg from './images/Searching.svg';
 import Button from '@mui/material/Button';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
-
 const SearchingALertComponent=()=>{
     return(
-            <Container maxWidth="xl" sx={{bgcolor: '#F4F4F4'}}>
-                <Box sx={{flexGrow: 1}}>
-                    <Grid container>
-                        <Grid xs={12} sm={6} md lg>
-                        <Typography variant="h4" sx={{fontFamily:'Playfair Display',fontSize:'64px'}}>
+            <Container maxWidth="xl" sx={{bgcolor: '#F4F4F4', margin:0, padding:'3vh 4vw 0 4vw'}} disableGutters>
+                <Grid container spacing={2}>
+                    <Grid xs={12} sm={6} md>
+                        <Typography variant="h4" sx={{fontFamily:'Playfair Display',fontSize:'4em'}}>
                             Find Your Perfect Apartment In here
                         </Typography>
-                        </Grid>
-                        <Grid xs={12} sm={6} md lg>
-                                <Box sx={{bgcolor: '#cfe8fc',height: '20vh' }}>
-                                    <div>
-                                    <Typography variant="caption" sx={{}}>
-                                        Discover affordable apartments  for university students
-                                    </Typography>
-                                    </div>
-                                    <div>
-                                        <Grid container>
-                                                <Grid xs={6} md={6} lg={6}>
-                                                <Button variant="outlined" startIcon={<SearchOutlinedIcon />} >
-                                                    Search
-                                                </Button>
-                                                <Button variant="contained" >
-                                                    Laern more
-                                                </Button>
-                                                </Grid>
-                                        </Grid>
-                                    </div>
-                                    
-                                </Box>
-                        </Grid>
-                        <Grid xs={12} sm={12} md lg>
-                            <img src={SearchingImg} alt='searching'/>
+                    </Grid>
+                    <Grid xs={12} sm={6} md sx={{paddingTop:'5vh'}}>
+                        <Typography sx={{fontSize:'1.1em'}}>
+                            Discover affordable apartments  for university students
+                        </Typography>
+                        <Grid container spacing={4} sx={{}}>
+                                <Grid xs={6}>
+                                    <Button variant="contained" color='primary' startIcon={<SearchOutlinedIcon />} >
+                                        Search
+                                    </Button>
+                                </Grid>
+                                <Grid xs={6}>
+                                    <Button variant="outlined">
+                                        Learn more
+                                    </Button>
+                                </Grid>
                         </Grid>
                     </Grid>
-                </Box>
+                    <Grid xs={12} sm={12} md sx={{textAlign:'center'}}>
+                        <img src={SearchingImg} alt='searching'/>
+                    </Grid>
+                </Grid>
             </Container>
     )
 }
