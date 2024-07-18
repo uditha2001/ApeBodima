5e307c961ab9cfa22e65a79a84b31574c789c5be
Sanjaya pradeep <katuwawala12365@usci.ruh.ac.lk>
2024-07-18 12:36:43 +0530

    Creat Font style class

--- webapp-frontend/src/pages/Home.jsx
+++ webapp-frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-// import HeaderComponent from "../component/HeaderComponent";
+import HeaderComponent from "../component/HeaderComponent";
 import SearchingALertComponent from '../component/SearchingAlertComponent';
+import FooterComponent from '../component/FooterComponent';
 
 const App=()=>{
     return(
         <div>
+            <HeaderComponent/>
             <SearchingALertComponent/>
+            <FooterComponent/>
         </div>
     )
 }
