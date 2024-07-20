eda145129b017eb842029ace441589c152c776f8
uditha <udithaimesh4@gmail.com>
2024-07-20 20:52:13 +0530

    re create header component with material ui

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Signup from './pages/Signup';
+import React from "react";
+import Signup from "./pages/Signup";
+import HeaderComponent from "./component/HeaderComponent";
+import SearchingAlertComponent from "./component/SearchingAlertComponent";
+import FooterComponent from "./component/FooterComponent";
 
-const App=()=>{
-    return(
-        <div>
-            <Signup/>
-        
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+};
 
 export default App;
--- webapp-frontend/src/component/HeaderComponent.js
+++ webapp-frontend/src/component/HeaderComponent.js
@@ -1,58 +1,107 @@
-import './css/Header.css'
-import logo from './images/logo.png'
-import logout from './images/login.png'
-import { useState } from 'react'
-const HeaderComponent=()=>{
-            const [sideBar,setSideBar]=useState(false);
+import "./css/Header.css";
+import logo from "./images/logo.png";
+import logout from "./images/login.png";
+import { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import { Button, Grid, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
-            return(
-                <header className="header">
-                    <div className='logodiv'>
-                      <img src={logo} alt="logo"/>
-                
-                    </div>
-                   
-                    <nav className="navbar">
-                        <ul>
-                            <li><a href="#explore">Explore</a></li>
-                            <li><a href="#addPlace">Add Place</a></li>
-                            <li><a href="#userAccount">User Account</a></li>
-                            <li><a href="#viewFavourite">View favorite</a></li>
-                            <li className='logoutClass'><a href="#logoutId"><img src={logout} alt="logout"/><span>Login in</span></a></li>
-                            <li className='signin'><a href="#signInId">Sign in</a></li> 
-                        </ul>
-                        <img className="menuBar" src="https://www.pinclipart.com/picdir/middle/532-5328945_menu-bar-icon-white-clipart-png-download-menu.png" alt="menubar"
-                            onClick={()=>{
-                                setSideBar(true);
-                            }}
-                        />
-                    </nav>
+const HeaderComponent = () => {
+  const [sideBar, setSideBar] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    let i=0;
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      if(width>=1040){
+        setSideBar(false);
+      }
+     
+    };
+    
+    // Add event listener on mount
+    window.addEventListener("resize", handleResize);
+    // Cleanup the event listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  });
 
-                   
-                    <nav className="sideNavBar" style={{
-                                right:`${sideBar ? "109px":"-200px"}`
-                    }} >
-                            <img className="imageList" 
-                                src="https://www.pinclipart.com/picdir/middle/35-356107_close-button-icon-png-clipart.png" 
-                                alt="close"
-                                onClick={()=>{
-                                    setSideBar(false);
-                                }}
-                            />
-                            <ul>
-                            <li><a href="#explore">Explore</a></li>
-                            <li><a href="#addPlace">Add Place</a></li>
-                            <li><a href="#userAccount">User Account</a></li>
-                            <li><a href="#viewFavourite">View favorite</a></li>
-                            <li className='logoutClass'><a href="#logoutId"><img src={logout} alt="logout"/><span>Login in</span></a></li>
-                            <li className='signin'><a href="#signInId">Sign in</a></li>
-                        </ul>
-                            
-                        </nav>
-                        
+  return (
+    <div>
+      <header className="header" >
+        <Container maxWidth="xl" className="container">
+          <Grid container className="navbar">
+            <div className="logodiv">
+              <img src={logo} alt="logo" />
+            </div>
+            <Button variant="text">
+              <a href="#explore">Explore</a>
+            </Button>
+            <Button variant="text">
+              <a href="#addPlace">Add Place</a>
+            </Button>
+            <Button variant="text">
+              <a href="#userAccount">User Account</a>
+            </Button>
+            <Button variant="text">
+              <a href="#viewFavourite">View favorite</a>
+            </Button>
+            <Button variant="outlined" maxWidth="20px" className="logout">
+              <img src={logout} alt="logout" />
+              <a href="#logoutId">
+                <span>Login in</span>
+              </a>
+            </Button>
+            <Button variant="outlined" className="signin">
+              <a href="#signInId">Sign in</a>
+            </Button>
+          </Grid>
+        </Container>
 
-                </header>
-            )
-}
+        <MenuIcon
+          className="menubar"
+          onClick={() => {
+            setSideBar(true);
+          }}
+        ></MenuIcon>
+      </header>
+      <div
+        className="container2"
+        style={{ right: `${sideBar ? "0px" : "-2000px"}`}}
+      >
+        <IconButton
+          onClick={() => {
+            setSideBar(false);
+          }}
+        >
+          <CloseRoundedIcon></CloseRoundedIcon>
+        </IconButton>
+        <Grid container className="navbar2" direction={"column"}>
+          <Button variant="text">
+            <a href="#explore">Explore</a>
+          </Button>
+          <Button variant="text">
+            <a href="#addPlace">Add Place</a>
+          </Button>
+          <Button variant="text">
+            <a href="#userAccount">User Account</a>
+          </Button>
+          <Button variant="text">
+            <a href="#viewFavourite">View favorite</a>
+          </Button>
+          <Button variant="outlined" maxWidth="20px" className="logout">
+            <img src={logout} alt="logout" />
+            <a href="#logoutId">
+              <span>Login in</span>
+            </a>
+          </Button>
+          <Button variant="outlined" className="signin">
+            <a href="#signInId">Sign in</a>
+          </Button>
+        </Grid>
+      </div>
+    </div>
+  );
+};
 
 export default HeaderComponent;
