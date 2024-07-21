591d3b139aff5b4dba08f248f91e45cdb7621338
uditha <udithaimesh4@gmail.com>
2024-07-22 00:37:14 +0530

    add checking password comparision

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Register from "./pages/Register";
+import SearchingAlertComponent from "./component/SearchingAlertComponent"
 
 const App = () => {
   return (
--- webapp-frontend/src/pages/Register.jsx
+++ webapp-frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Box, TextField, Container, Typography, Button } from "@mui/material";
 import "./css/Register.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import sideImg from "../component/images/signupImg/Other 07.png";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -9,7 +9,24 @@ import Lock from "@mui/icons-material/Lock";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 const Register = () => {
   const [showPass, setShowPass] = useState(false);
-  const [firstPassShow,setfirstPassShow]=useState(false);
+  const [firstPassShow, setfirstPassShow] = useState(false);
+  const [password1,setPassword1]=useState(null);
+  const [password2,setPassword2]=useState(null);
+  const [comaprision,setComparition]=useState(true);
+  const [errors,setError]=useState(false);
+  useEffect(
+    ()=>{
+      
+      if(password1===password2 || password2==null){
+        setComparition(true); 
+        setError(false);    
+      }
+      else{
+        setComparition(false);
+        setError(true);
+      }
+    }
+  )
   return (
     <Container
       className="container"
@@ -25,7 +42,7 @@ const Register = () => {
       }}
     >
       <Box
-        classname="imageSide"
+        className="imageSide"
         sx={{ bgcolor: "#EBEFFF" }}
         width={366}
         height={"auto"}
@@ -38,7 +55,7 @@ const Register = () => {
         />
       </Box>
       <Box
-        classname="signupSide"
+        className="signupSide"
         width={366}
         height={517}
         sx={{
@@ -98,25 +115,34 @@ const Register = () => {
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton sx={{ cursor: "pointer" }} onClick={() => {
-                    if(firstPassShow==true){
-                      setfirstPassShow(false)
-                    }
-                    else{
-                      setfirstPassShow(true)
-                    }
-                   }}>
-                    {firstPassShow? <VisibilityOutlinedIcon/>:<VisibilityOffOutlinedIcon/> }
+                  <IconButton
+                    sx={{ cursor: "pointer" }}
+                    onClick={() => {
+                      if (firstPassShow == true) {
+                        setfirstPassShow(false);
+                      } else {
+                        setfirstPassShow(true);
+                      }
+                    }}
+                  >
+                    {firstPassShow ? (
+                      <VisibilityOutlinedIcon />
+                    ) : (
+                      <VisibilityOffOutlinedIcon />
+                    )}
                   </IconButton>
                 </InputAdornment>
               ),
             }}
             className="custom-textfield"
-            type={firstPassShow? "text" :"password"}
+            type={firstPassShow ? "text" : "password"}
             fullWidth
             variant="outlined"
             sx={{ position: "relative", height: "8px", width: "346px" }}
             size="small"
+            onChange={(e)=>{
+                    setPassword1(e.target.value);
+            }}  
           />
         </div>
         <div style={{ marginTop: "30px", position: "relative" }}>
@@ -125,32 +151,43 @@ const Register = () => {
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton sx={{ cursor: "pointer" }} onClick={() => {
-                    if(showPass==true){
-                      setShowPass(false)
-                    }
-                    else{
-                      setShowPass(true)
-                    }
-                   }}>
-                    {showPass? <VisibilityOutlinedIcon/>:<VisibilityOffOutlinedIcon/> }
+                  <IconButton
+                    sx={{ cursor: "pointer" }}
+                    onClick={() => {
+                      if (showPass == true) {
+                        setShowPass(false);
+                      } else {
+                        setShowPass(true);
+                      }
+                    }}
+                  >
+                    {showPass ? (
+                      <VisibilityOutlinedIcon />
+                    ) : (
+                      <VisibilityOffOutlinedIcon />
+                    )}
                   </IconButton>
                 </InputAdornment>
               ),
             }}
             className="custom-textfield"
-            type={showPass? "text":"password"}
+            type={showPass ? "text" : "password"}
             fullWidth
             variant="outlined"
             sx={{ position: "relative", height: "8px", width: "346px" }}
             size="small"
+            error={errors}
+            helperText={comaprision? "":"not match"}
+            onChange={(e)=>{
+              setPassword2(e.target.value);
+      }}
           />
         </div>
-        <div style={{ marginTop: "30px", position: "relative" }}>
+        <div style={{ marginTop: "45px", position: "relative" }} >
           <Button sx={{ bgcolor: "#9EC9F7", width: "346px" }}>Register</Button>
         </div>
-        <div>
-          <Typography sx={{ justifyContent: "space-between" }}>
+        <div className="register">
+          <Typography sx={{ justifyContent: "space-between",marginTop:"10px" }} className="loggintext">
             <spam>Yes i have an account?</spam>
             <a
               style={{
