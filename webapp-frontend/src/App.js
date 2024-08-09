6f0fcadfb37f218723991872ee83aa88675a88c2
uditha <udithaimesh4@gmail.com>
2024-08-09 23:45:37 +0530

    create a add boardim component

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -5,17 +5,28 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import AddBodim from "./pages/AddBodim";
+import BoardimCard from "./component/BoardimCard"
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/addBodim" element={<AddBodim />} />
-          <Route path="/a" element={<AboutUs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div
+      style={{
+        alignItems:"center",
+        position:"relative",
+        marginLeft:"10px"
+      }}
+    >
+      <BoardimCard
+      />
+    </div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<MainLayout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="/addBodim" element={<AddBodim />} />
+    //       <Route path="/a" element={<AboutUs />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
   );
 };
 
