7742060aca2b8e66745b2c8f2d622a0b241b3360
sasanka2023 <gayathra12350@usci.ruh.ac.lk>
2024-08-10 12:52:43 +0530

    change the app.js and filterbar component

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
-import MainLayout from "./mainLayout/MainLayout";
 import Register from "./pages/Register";
-<<<<<<< HEAD
 import SearchingAlertComponent from "./component/SearchingAlertComponent"
 import  Login  from "./pages/Login";
 import HeaderComponent from "./component/HeaderComponent";
@@ -15,24 +12,8 @@ const App = () => {
       {/* <Register></Register> */}
       
       <HeaderComponent/>
-      <FilterBarComponent/>
+      {/*removed addbodime component */}
     </div>
-=======
-import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
-import AddBodim from "./pages/AddBodim";
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/addBodim" element={<AddBodim />} />
-          <Route path="/a" element={<AboutUs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
->>>>>>> f1f27a44a2ab8b3822535d5db4253d1c34a6a657
   );
 };
 
