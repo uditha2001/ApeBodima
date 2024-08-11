ee6037346ea0ec1814399b6775b86aa931165b3d
Sanjaya Pradeep <152323246+SPradeepIndie@users.noreply.github.com>
2024-08-11 11:37:33 +0530

    Update App.js
    commit app.js

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainLayout from "./mainLayout/MainLayout";
-import Home from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
-import FilterBarComponent from "./component/FilterBarComponent";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import MainLayout from "./mainLayout/MainLayout";
+// import Home from "./pages/Home";
+// import AboutUs from "./pages/AboutUs";
+// import FilterBarComponent from "./component/FilterBarComponent";
 const App = () => {  
   return (
     <div>
