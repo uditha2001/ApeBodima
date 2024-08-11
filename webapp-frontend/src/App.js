5682439745b6feb657f7227c040b297bcc92979a
tharaka <tharakam2001@gmail.com>
2024-08-11 17:57:22 +0530

    Revert "remove warning in addbodim page"
    This reverts commit df5665456f18bd3d81b7cda2eb458e3eb1308977.

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import MainLayout from "./mainLayout/MainLayout";
-// import Home from "./pages/Home";
-// import AboutUs from "./pages/AboutUs";
-// import FilterBarComponent from "./component/FilterBarComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FilterBarComponent from "./component/FilterBarComponent";
+import MainLayout from "./mainLayout/MainLayout";
+import AboutUs from "./pages/AboutUs";
+import Home from "./pages/Home";
 const App = () => {  
   return (
     <div>
-      {/* <Register></Register> */}
-      
-      {/*<HeaderComponent/>*/}
-      {/*removed addbodime component */}
+
     </div>
   );
 };
