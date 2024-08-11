cbc39b197b783efd3df5c725182d9b084247f2e5
Udaya <shantha12393@gmail.com>
2024-08-11 17:54:30 +0530

    Removing all warnings inside the AboutUs page and FilterBar Component

--- webapp-frontend/src/App.js
+++ webapp-frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./mainLayout/MainLayout";
-import Register from "./pages/Register";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import AddBodim from "./pages/AddBodim";
+import FilterBar from "./component/FilterBarComponent";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,6 +13,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/addBodim" element={<AddBodim />} />
           <Route path="/a" element={<AboutUs />} />
+          <Route path="/filter" element={<FilterBar />} />
         </Route>
       </Routes>
     </BrowserRouter>
